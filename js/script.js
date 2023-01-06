c3e0efb8f05539a81c4799932defdd404ef5139c
Janos Tordai <tjanos88@gmail.com>
2023-01-06 23:26:06 +0100

    refactored two functions

--- js/script.js
+++ js/script.js
@@ -9,56 +9,72 @@ function capitalizeFirstLetter(string) {
   }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    let message = ''
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    let result = ''
 
-    if (playerSelection === computerSelection){
-        message = 'Draw!';
-    }else if (
-        playerSelection == 'paper' & computerSelection == 'rock' ||
-        playerSelection == 'scissors' & computerSelection == 'paper' ||
-        playerSelection == 'rock' & computerSelection == 'scissors'
+    if (player === computer){
+        result = 'Tie game!';
+    } else if (
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper') ||
+        (player === 'rock' && computer === 'scissors')
     ){
-        message = `You Won! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`;
-    }else{
-        message = `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`;
+        result = `You Won! ${capitalizeFirstLetter(player)} beats ${capitalizeFirstLetter(computer)}`;
+    } else {
+        result = `You Lose! ${capitalizeFirstLetter(computer)} beats ${capitalizeFirstLetter(player)}`;
     }
-    console.log(message);
-    return message;
+    console.log(result);
+    return result;
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    scoreBoard = {
-        player: playerScore,
-        computer: computerScore
-    };
-
-    for (let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice();
-        let playerSelection;
-        while (!choices.includes(playerSelection)){
-            playerSelection = prompt('Choose between Rock, Paper and Scissors! ');
-        };
-
-        const result = playRound(playerSelection, computerSelection);
-        if (result.includes('Won')){
-            scoreBoard.player++;
-        }else if(result.includes('Lose')){
-            scoreBoard.computer++;
-        }
-        console.log(`Current standings - Player: ${scoreBoard.player}, Computer: ${scoreBoard.computer}`);
+function initializeScoreBoard(){
+    return {
+        player: 0,
+        computer: 0
     }
-    if (scoreBoard.player > scoreBoard.computer){
+}
+
+function getPlayerChoice() {
+    let playerSelection;
+    while (!choices.includes(playerSelection)){
+        playerSelection = prompt('Choose between Rock, Paper and Scissors!');
+    }
+    return playerSelection;
+}
+
+function updateScoreboard(scoreBoard, result) {
+    if (result.includes('Won')){
+        scoreBoard.player++;
+    }else if(result.includes('Lose')){
+        scoreBoard.computer++;
+    }
+}
+
+function displayGameResult(scoreboard) {
+    if (scoreboard.player > scoreboard.computer){
         console.log('** You Won! Game Over **');
-    }else if (scoreBoard.player > scoreBoard.computer){
+    }else if (scoreboard.player > scoreboard.computer){
         console.log('** You Lose! Game Over **');
     }else{
         console.log('** Draw! Game Over **')
     }
+}
+
+function displayStandings(scoreboard) {
+    console.log(`Current standings - Player: ${scoreboard.player}, Computer: ${scoreboard.computer}`);
+}
+
+function game(){
+    let scoreBoard = initializeScoreBoard();
+    for (let i = 0; i < 3; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        updateScoreboard(scoreBoard, result);
+        displayStandings(scoreBoard);
+    }
+    displayGameResult(scoreBoard);
 }
 
 game();
