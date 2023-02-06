f3368c0accca7e58e6177a3554958e89527baf70
Janos Tordai <tjanos88@gmail.com>
2023-02-06 21:53:46 +0100

    MAINT: refactored for ui usage

--- js/script.js
+++ js/script.js
@@ -1,80 +1,85 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+let scoreBoard = initializeScoreBoard();
 
-function getComputerChoice(){
-    return choices[Math.floor(Math.random()*choices.length)];
-}
+document.querySelectorAll('button').forEach(item => {
+    item.addEventListener('click', playRound)
+});
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
 
-function playRound(playerSelection, computerSelection){
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-    let result = ''
-
-    if (player === computer){
-        result = 'Tie game!';
-    } else if (
-        (player === 'paper' && computer === 'rock') ||
-        (player === 'scissors' && computer === 'paper') ||
-        (player === 'rock' && computer === 'scissors')
-    ){
-        result = `You Won! ${capitalizeFirstLetter(player)} beats ${capitalizeFirstLetter(computer)}`;
-    } else {
-        result = `You Lose! ${capitalizeFirstLetter(computer)} beats ${capitalizeFirstLetter(player)}`;
-    }
-    console.log(result);
-    return result;
-}
-
-function initializeScoreBoard(){
+function initializeScoreBoard() {
     return {
         player: 0,
         computer: 0
     }
 }
 
-function getPlayerChoice() {
-    let playerSelection;
-    while (!choices.includes(playerSelection)){
-        playerSelection = prompt('Choose between Rock, Paper and Scissors!');
-    }
-    return playerSelection;
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function playRound() {
+
+    const player = this.value;
+    const computer = getComputerChoice();
+    let result = '';
+
+    if (player === computer) {
+        result = 'Tie game this round!';
+    } else if (
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper') ||
+        (player === 'rock' && computer === 'scissors')
+    ) {
+        result = `You won this round! ${capitalizeFirstLetter(player)} beats ${capitalizeFirstLetter(computer)}`;
+    } else {
+        result = `You lose this round! ${capitalizeFirstLetter(computer)} beats ${capitalizeFirstLetter(player)}`;
+    }
+
+    document.querySelector(".current-result").innerHTML = result;
+    updateScoreboard(scoreBoard, result);
+    displayStandings(scoreBoard);
+
+    if (scoreBoard.player === 5 || scoreBoard.computer === 5) {
+        displayGameResult(scoreBoard);
+        addRestartButton();
+        return
+    }
+}
+
+
 function updateScoreboard(scoreBoard, result) {
-    if (result.includes('Won')){
+    if (result.includes('won')) {
         scoreBoard.player++;
-    } else if (result.includes('Lose')){
+    } else if (result.includes('lose')) {
         scoreBoard.computer++;
     }
 }
 
+
 function displayGameResult(scoreboard) {
-    if (scoreboard.player > scoreboard.computer){
-        console.log('** You Won! Game Over **');
-    } else if (scoreboard.player > scoreboard.computer){
-        console.log('** You Lose! Game Over **');
+    if (scoreboard.player > scoreboard.computer) {
+        document.querySelector('.final-result').innerHTML = '** You Won! Game Over **';
     } else {
-        console.log('** Draw! Game Over **')
+        document.querySelector('.final-result').innerHTML = '** You Lose! Game Over **';
     }
 }
+
+function addRestartButton() {
+
+    let restartButton = document.createElement('button');
+    restartButton.innerHTML = 'Restart game';
+    document.querySelector('.final-result').append(restartButton);
+    restartButton.addEventListener('click', () => { window.location.reload() });
+}
+
 
 function displayStandings(scoreboard) {
-    console.log(`Current standings - Player: ${scoreboard.player}, Computer: ${scoreboard.computer}`);
+    document.querySelector('.scoreboard').innerHTML = `Current standings - Player: ${scoreboard.player}, Computer: ${scoreboard.computer}`;
 }
-
-function game(){
-    let scoreBoard = initializeScoreBoard();
-    for (let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        updateScoreboard(scoreBoard, result);
-        displayStandings(scoreBoard);
-    }
-    displayGameResult(scoreBoard);
-}
-
-game();
